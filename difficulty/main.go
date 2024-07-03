7b317b914eacb99c24f56398853d2d58a150c919
hunjixin <16705420332lee@gmai.com>
2024-07-03 02:58:54 +0000

    fix

--- difficulty/main.go
+++ difficulty/main.go
@@ -8,19 +8,20 @@ import (
 )
 
 var (
+	BlockInterval = 12
 	uint256Max, _ = new(big.Float).SetString("115792089237316195423570985008687907853269984665640564039457584007913129639935")
 )
 
-// 40m 74061421902600066637965639403500996888198243478266023770329538625536
+// 40m 92576780592126171815437600338300430792573009392238517278497593884672
 func main() {
-	targetDifficulty := esitimateDifficulty(1000, 30, 0.00001)
+	targetDifficulty := esitimateDifficulty(40, 30, 0.00001)
 
-	number := esitimateHashRate(targetDifficulty, 30*12, 0.00001)
+	number := esitimateHashRate(targetDifficulty, 30*BlockInterval, 0.00001)
 	fmt.Print(number)
 }
 
 func esitimateDifficulty(power int, windowsSize int, probability float64) *big.Float {
-	n := int64(power * 1000 * 1000 * windowsSize * 12)
+	n := int64(power * 1000 * 1000 * windowsSize * BlockInterval)
 
 	targetProb := new(big.Float).SetFloat64(probability)
 
@@ -45,6 +46,6 @@ func esitimateHashRate(difficulty *big.Float, durS int, probability float64) *bi
 	tmp := new(big.Float).Mul(big.NewFloat(float64(durS)), bigfloat.Log(not_probability))
 	hashRate := new(big.Float).Quo(bigfloat.Log(targetProb), tmp)
 	hashRate = hashRate.Quo(hashRate, big.NewFloat(1000*1000))
-	fmt.Println(hashRate.Text('f', 0))
+	fmt.Println(hashRate.Text('f', 2))
 	return hashRate
 }
